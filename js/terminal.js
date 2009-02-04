fca57f69ed721de777d873454f3a12f57ca1b9ea
Prateek Saxena <prtksxna@gmail.com>
2009-02-04 18:26:48 +0530

    Made some interface changes

--- js/terminal.js
+++ js/terminal.js
@@ -1,27 +1,74 @@
+GIT_USER = "prtksxna";
+
 Terminal = Class.create({
 	initialize: function(user){
 		this.user = user;
-		this.keeper = new Element('div', {'id': 'terminal_keeper' }); //Create a container where
-    $(document.body).insert(this.keeper);
-		this.keeper.update("Welcome to ~\/github\/"+this.user);
-		this.whois = $H();
-		
-		
-		
-		new Ajax.Request('http://github.com/api/v1/json/defunkt/',{
-			method:'get',
-			onCreate: function(){
-				alert('a request has been initialized!');
-				alert(this);
-				console.log(this);
-			},
-			onSuccess: function(transport){
-				var response = transport.responseText || "no response text";
-				alert(response);
-			},
-			onFailure: function(){ alert('Something went wrong...') }
-		});
-		
-		
+		this.commands = new Hash;
 	},
-})
+	connection: function(data){
+		this.userdata = data
+		$("terminal_console").update("Welcome to ~\/github\/<a href=\'http://github.com/"+this.userdata.user.login+"\'>"+this.userdata.user.login+"<\/a>\/ <br\/> <br\/>");
+		this.loc = "~/github/"+this.userdata.user.login
+		$("terminal_folder").update(this.loc+"$<span class=\'loading\'>><\/span>");
+		$("terminal_input").show().activate().value = "";
+		this.addDefaultCommands();
+	},
+	addData: function(element){
+		$("terminal_console").insert({bottom: element});
+	},
+	addDefaultCommands: function(){
+		this.commands.set('whois',{help:'Gives you information about the user.',exec: function(){
+			var whois = new Element('div',{class:'info'}).update(git.userdata.user.name+", "+git.userdata.user.company+" ("+git.userdata.user.login+") \t <a href=\'"+git.userdata.user.blog+"\'>"+git.userdata.user.blog+"<\/a> <br> <a class=\'green\' href=\'mailto:"+git.userdata.user.email+"\'>"+git.userdata.user.email+"<\/a>");
+			git.addData(whois);
+		}});
+		
+		
+		this.commands.set('ls',{help:'Lists all the repositories by this user.', exec: function(){
+			var repos = git.userdata.user.repositories;
+			var repEl = new Element('div', {class: 'ls-list'});
+			var list = "<table>";
+			repos.each(function(el) {
+				list += "<tr><td>"+el.name+"<\/td><td><a class=\'yellow\' href=\'"+el.url+"\'>"+el.url+"<\/a><\/td><\/tr>"
+			});
+			list += "<\/table>"
+			repEl.update(list);
+			git.addData(repEl);
+		}});
+	},
+	exec: function(command){
+		var comEl = new Element('span', {class:'com'});
+		comEl.update(""+ this.loc +">" + command);
+		this.addData(comEl);
+		var com = (this.commands.get(command));
+
+		if (!com){
+			this.addData("<br \/>No such command <br \/>");
+		}else{
+			com.exec();
+		}
+		
+		$("terminal_input").value = "";
+		Effect.ScrollTo("terminal_input");
+	},
+	getData: function(url,callback){
+		var sc = new Element('script', {src:url+"="+callback});
+		$(document.body).insert(sc);
+	},
+});
+
+git = new Terminal(GIT_USER); 
+
+document.observe('dom:loaded',function(e) {
+	var sc = new Element('script', {src:'http://github.com/api/v1/json/'+GIT_USER+'?callback=initTerm'});
+	$(document.body).insert(sc);
+	
+	$("term_in").observe('submit',function(e) {
+		e.stop()
+		git.exec($F("terminal_input"));
+	});
+	
+});
+
+initTerm = function(data) {
+	git.connection(data);
+};
